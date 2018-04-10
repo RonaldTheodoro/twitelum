5f52eb3c939f0b9b09dbdd9e2be43740ddd2d04c
Ronald Theodoro <ronald.silva4@fatec.sp.gov.br>
2018-04-09 21:53:50 -0300

    add cabecalho

--- twitelum-front/src/App.js
+++ twitelum-front/src/App.js
@@ -1,12 +1,13 @@
 import React, { Component } from 'react';
 // import logo from './logo.svg';
+import Cabecalho from './componentes/Cabecalho'
 import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        Em construção...
+        <Cabecalho />
       </div>
     );
   }
