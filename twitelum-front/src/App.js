a7fcbc3645d7a06e57156fe89fc72b4edf712ac8
Ronald Theodoro <ronald.silva4@fatec.sp.gov.br>
2018-04-10 20:42:12 -0300

    add text validation

--- twitelum-front/src/App.js
+++ twitelum-front/src/App.js
@@ -7,37 +7,53 @@ import TrendsArea from './components/TrendsArea'
 import Tweet from './components/Tweet'
 
 class App extends Component {
-  render() {
+  constructor () {
+    super()
+    this.state = {
+      novoTweet: ''
+    }
+  }
+
+  render () {
     return (
       <Fragment>
         <Cabecalho>
-            <NavMenu usuario="@omariosouto" />
+          <NavMenu usuario="@omariosouto" />
         </Cabecalho>
         <div className="container">
-            <Dashboard>
-                <Widget>
-                    <form className="novoTweet">
-                        <div className="novoTweet__editorArea">
-                            <span className="novoTweet__status">0/140</span>
-                            <textarea className="novoTweet__editor" placeholder="O que está acontecendo?"></textarea>
-                        </div>
-                        <button type="submit" className="novoTweet__envia">Tweetar</button>
-                    </form>
-                </Widget>
-                <Widget>
-                    <TrendsArea />
-                </Widget>
-            </Dashboard>
-            <Dashboard posicao="centro">
-                <Widget>
-                    <div className="tweetsArea">
-                        <Tweet />
-                    </div>
-                </Widget>
-            </Dashboard>
+          <Dashboard>
+            <Widget>
+              <form className="novoTweet">
+                <div className="novoTweet__editorArea">
+                  <span className={`novoTweet__status ${ this.state.novoTweet.length > 140 ? 'novoTweet__status--invalido' : '' }` }>
+                    { this.state.novoTweet.length }/140
+                    </span>
+                  <textarea
+                    value={ this.state.novoTweet }
+                    onChange={(event) => {
+                      this.setState({novoTweet: event.target.value})
+                    }}
+                    className="novoTweet__editor"
+                    placeholder="O que está acontecendo?">
+                  </textarea>
+                </div>
+                <button type="submit" disabled={ this.state.novoTweet.length > 140 ? true : false } className="novoTweet__envia">Tweetar</button>
+              </form>
+            </Widget>
+            <Widget>
+              <TrendsArea />
+            </Widget>
+          </Dashboard>
+          <Dashboard posicao="centro">
+            <Widget>
+              <div className="tweetsArea">
+                <Tweet />
+              </div>
+            </Widget>
+          </Dashboard>
         </div>
       </Fragment>
-    );
+    )
   }
 }
 
