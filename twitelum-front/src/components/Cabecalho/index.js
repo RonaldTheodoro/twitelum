8709f8e804963c496dcebed8397acc44d526d043
RonaldTheodoro <ronald.silva4@fatec.sp.gov.br>
2018-04-12 19:57:28 -0300

    use 2 spaces to indent

--- twitelum-front/src/components/Cabecalho/index.js
+++ twitelum-front/src/components/Cabecalho/index.js
@@ -3,18 +3,18 @@ import './cabecalho.css'
 // import './navMenu.css'
 
 class Cabecalho extends Component {
-    render() {
-        return (
-            <header className="cabecalho">
-                <div className="cabecalho__container container">
-                    <h1 className="cabecalho__logo">
-                        <a href="">Twitelum</a>
-                    </h1>
-                    { this.props.children }
-                </div>
-            </header>
-        )
-    }
+  render() {
+    return (
+      <header className="cabecalho">
+        <div className="cabecalho__container container">
+          <h1 className="cabecalho__logo">
+            <a href="">Twitelum</a>
+          </h1>
+          { this.props.children }
+        </div>
+      </header>
+    )
+  }
 }
 
 export default Cabecalho
