1a560eff097d89b0c6fdb0a113a13fcb65ea2ee9
RonaldTheodoro <ronald.silva4@fatec.sp.gov.br>
2018-04-12 19:33:57 -0300

    implement LoginPage

--- twitelum-front/src/pages/LoginPage/index.js
+++ twitelum-front/src/pages/LoginPage/index.js
@@ -4,36 +4,90 @@ import Widget from '../../components/Widget'
 import './loginPage.css'
 
 
+function MensagemErro(props) {
+  if (props.errorMessage) {
+    return (
+      <div className="loginPage__errorBox">
+        {props.errorMessage}
+      </div>
+    )
+  } else {
+    return ''
+  }
+}
+
+
 class LoginPage extends Component {
-    render() {
-        return (
-            <div className="loginPage">
-                <div className="container">
-                    <Widget>
-                        <h1 className="loginPage__title">Twitelum</h1>
-                        <form className="loginPage__form" action="/">
-                            <div className="loginPage__inputWrap">
-                                <label className="loginPage__label" htmlFor="login">Login</label> 
-                                <input className="loginPage__input" type="text" id="login" name="login"/>
-                            </div>
-                            <div className="loginPage__inputWrap">
-                                <label className="loginPage__label" htmlFor="senha">Senha</label> 
-                                <input className="loginPage__input" type="password" id="senha" name="senha"/>
-                            </div>
-                            {/* <div className="loginPage__errorBox">
-                                Mensagem de erro!
-                            </div> */}
-                            <div className="loginPage__inputWrap">
-                                <button className="loginPage__btnLogin" type="submit">
-                                    Logar
-                                </button>
-                            </div>
-                        </form>
-                    </Widget>
-                </div>
-            </div>
-        )
+  state = {
+    errorMessage: ''
+  }
+
+  fazerLogin = (event) => {
+    event.preventDefault()
+
+    const dadosDeLogin = {
+      login: this.inputLogin.value,
+      senha: this.inputSenha.value
     }
+
+    fetch('http://localhost:3001/login', {
+      method: 'POST',
+      body: JSON.stringify(dadosDeLogin)
+    }).then((response) => {
+      if (!response.ok)
+        throw response;
+      return response.json()
+    }).then((responseEmJson) => {
+      localStorage.setItem('TOKEN', responseEmJson.token)
+      this.props.history.push('/')
+    }).catch((responseError) => {
+      responseError.json().then((response) => {
+        this.setState({
+          errorMessage: response.message
+        })
+      })
+    })
+  }
+
+  MensagemErro() {
+    if (this.state.errorMessage)
+      return (
+        <div className="loginPage__errorBox">
+          {this.state.errorMessage}
+        </div>
+      )
+    else
+      return ''
+
+  }
+
+  render() {
+    return (
+      <div className="loginPage">
+        <div className="container">
+          <Widget>
+            <h1 className="loginPage__title">Twitelum</h1>
+            <form className="loginPage__form" action="/" onSubmit={this.fazerLogin}>
+              <div className="loginPage__inputWrap">
+                <label className="loginPage__label" htmlFor="login">Login</label>
+                <input ref={(inputLogin) => this.inputLogin = inputLogin} className="loginPage__input" type="text" id="login" name="login" />
+              </div>
+              <div className="loginPage__inputWrap">
+                <label className="loginPage__label" htmlFor="senha">Senha</label>
+                <input ref={(inputSenha) => this.inputSenha = inputSenha} className="loginPage__input" type="password" id="senha" name="senha" />
+              </div>
+              <MensagemErro errorMessage={this.state.errorMessage} />
+              <div className="loginPage__inputWrap">
+                <button className="loginPage__btnLogin" type="submit">
+                  Logar
+                </button>
+              </div>
+            </form>
+          </Widget>
+        </div>
+      </div>
+    )
+  }
 }
 
 
