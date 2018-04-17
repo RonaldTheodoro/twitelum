10ab448184eb3b597101dec5b22d6dd032ef1119
RonaldTheodoro <ronald.silva4@fatec.sp.gov.br>
2018-04-17 20:23:30 -0300

    add ADICIONA_TWEET subscriber

--- twitelum-front/src/store.js
+++ twitelum-front/src/store.js
@@ -1,12 +1,16 @@
-import { createStore } from 'redux'
-
+import { createStore, applyMiddleware } from 'redux'
+import thunk from 'redux-thunk'
 
 function tweetsReducer(state = [], action = {}) {
   if (action.type === 'CARREGA_TWEETS')
     return action.tweets
+
+  if (action.type === 'ADICIONA_TWEET')
+    return [action.tweet, ...state]
+
   return state
 }
 
-const store = createStore(tweetsReducer)
+const store = createStore(tweetsReducer, applyMiddleware(thunk))
 
 export default store
