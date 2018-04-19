81e5b6a1ce83c777de738b6f58a4a478d47b8699
RonaldTheodoro <ronald.silva4@fatec.sp.gov.br>
2018-04-18 21:50:56 -0300

    remove all logic from Tweet

--- twitelum-front/src/components/Tweet/index.js
+++ twitelum-front/src/components/Tweet/index.js
@@ -5,26 +5,6 @@ import './tweet.css'
 
 
 class Tweet extends Component {
-  constructor(props) {
-    super()
-    this.state = {
-      likeado: props.tweetInfo.likeado,
-      totalLikes: props.tweetInfo.totalLikes,
-    }
-  }
-
-  handleLike = (tweetId) => {
-    const { likeado, totalLikes } = this.state
-    this.setState({
-      likeado: !likeado,
-      totalLikes: likeado ? totalLikes - 1 : totalLikes + 1
-    })
-    const token = localStorage.getItem('TOKEN')
-    const url = `http://localhost:3001/tweets/${tweetId}/like?X-AUTH-TOKEN=${token}`
-    fetch(url, { method: 'POST' })
-      .then(response => response.json())
-  }
-
   render() {
     return (
       <article className="tweet" onClick={this.props.handleAbreModalParaTweet}>
@@ -41,8 +21,8 @@ class Tweet extends Component {
         </div>
         <p className="tweet__conteudo">{this.props.tweetInfo.conteudo}</p>
         <footer className="tweet__footer">
-          <button className="btn btn--clean" onClick={(event) => this.handleLike(this.props.tweetInfo._id)}>
-            <svg className={`icon icon--small iconHeart ${this.state.likeado && 'iconHeart--active'}`} xmlns="http://www.w3.org/2000/svg" viewBox="0 0 47.5 47.5">
+          <button className="btn btn--clean" onClick={this.props.handleLike}>
+            <svg className={`icon icon--small iconHeart ${this.props.tweetInfo.likeado && 'iconHeart--active'}`} xmlns="http://www.w3.org/2000/svg" viewBox="0 0 47.5 47.5">
               <defs>
                 <clipPath id="a">
                   <path d="M0 38h38V0H0v38z"></path>
@@ -52,9 +32,9 @@ class Tweet extends Component {
                 <path d="M36.885 25.166c0 5.45-4.418 9.868-9.867 9.868-3.308 0-6.227-1.632-8.018-4.128-1.79 2.496-4.71 4.129-8.017 4.129-5.45 0-9.868-4.418-9.868-9.868 0-.773.098-1.52.266-2.242C2.75 14.413 12.216 5.431 19 2.965c6.783 2.466 16.249 11.448 17.617 19.96.17.721.268 1.47.268 2.241"></path>
               </g>
             </svg>
-            {this.state.totalLikes}
+            {this.props.tweetInfo.totalLikes}
           </button>
-          
+
           {this.props.tweetInfo.removivel &&
             <button onClick={this.props.removeHandler} className="btn btn--blue btn--remove">X</button>}
         </footer>
